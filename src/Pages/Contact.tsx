0e8b7f1283ddb420371aacb691202b96a258d61f
Jenny Pieloor <devjenny.official@gmail.com>
2024-09-06 19:16:46 +0800

    Added static UI for Contact page.

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,11 +1,17 @@
 import Hero from "../components/Contact/Hero";
+import Footer from "../components/Footer";
+import SocialLogos from "../components/SocialLogos";
 
 const Contact = () => {
-  return <div id="contact" className="w-full">
-  <div className="p-14">
-    <Hero />
-  </div>
-</div>
+  return (
+    <div id="contact" className="w-full">
+      <div className="p-14">
+        <Hero />
+        <SocialLogos />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/Contact/Hero.tsx
+++ src/components/Contact/Hero.tsx
@@ -1,13 +1,13 @@
-
-
 const Hero = () => {
   return (
-    <div className="flex flex-col w-full gap-14">
-      
-
-      
+    <div className="flex flex-col justify-center items-center w-full h-[27rem] border border-secondary dark:border-primaryDark/60 rounded-3xl uppercase gap-[5px] text-secondary dark:text-primaryDark">
+      <p className="font-medium text-xl">Got project in mind?</p>
+      <p className="font-bold text-[3.8rem]">Let’s Connect</p>
+      <button className="py-2 px-6 border rounded-xl mt-10">
+        <p className="font-medium text-xl uppercase">Let’s Talk</p>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
